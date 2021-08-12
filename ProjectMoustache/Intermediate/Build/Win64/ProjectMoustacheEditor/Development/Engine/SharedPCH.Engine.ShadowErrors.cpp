7e3c966f0760a2a9424667f6a28bfa8845f8a6c1
pbrodie94 <82655776+pbrodie94@users.noreply.github.com>
2021-08-12 11:34:54 -0400

    Initial Commit
    - Project Created

---
+++ ProjectMoustache/Intermediate/Build/Win64/ProjectMoustacheEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30038
+#include "F:/Game Development/Unreal Projects/ProjectMoustache_TFSCapstone2021-2022/ProjectMoustache/Intermediate/Build/Win64/ProjectMoustacheEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ ProjectMoustache/Source/ProjectMoustache/ProjectMoustacheGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectMoustacheGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectMoustacheGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectMoustacheGameMode();
+};
+
+
+
