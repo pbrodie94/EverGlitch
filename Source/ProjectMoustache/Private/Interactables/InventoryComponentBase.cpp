1636d5cfc3aa6c0f939b27085c921921b7cdaf35
pbrodie94 <82655776+pbrodie94@users.noreply.github.com>
2021-12-29 17:36:04 -0500

    Moving inventory to cpp

--- Source/ProjectMoustache/Private/Interactables/InventoryComponentBase.cpp
+++ Source/ProjectMoustache/Private/Interactables/InventoryComponentBase.cpp
@@ -19,7 +19,19 @@ void UInventoryComponentBase::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
+
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if (InputComponent)
+	{
+		InputComponent->BindAction("QuickSlot1", IE_Pressed, this, &UInventoryComponentBase::UseQuickSlot1);
+		InputComponent->BindAction("QuickSlot2", IE_Pressed, this, &UInventoryComponentBase::UseQuickSlot2);
+		InputComponent->BindAction("QuickSlot3", IE_Pressed, this, &UInventoryComponentBase::UseQuickSlot3);
+	} else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Error: InventoryComponentBase failed to get input component reference."));
+	}
+
+	inventory.SetNum(numberOfSlots);
 }
 
 
@@ -29,4 +41,272 @@ void UInventoryComponentBase::TickComponent(float DeltaTime, ELevelTick TickType
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+}
+
+bool UInventoryComponentBase::AddToInventory(FInventorySlot itemToAdd)
+{
+	if (!itemToAdd.item.isStackable)
+	{
+		return CreateStack(itemToAdd);
+	}
+
+	if (GetHasPartialStack(itemToAdd))
+	{
+		return AddToStack(itemToAdd, GetPartialStackIndex(itemToAdd));
+	}
+
+	return CreateStack(itemToAdd);
+}
+
+/**
+* Creates a new stack of items
+* Does not add the item if inventory is full
+* Adds multiple items if the item is stackable and has more than 1
+* Otherwise, just adds one of the item
+*/
+bool UInventoryComponentBase::CreateStack(FInventorySlot itemToAdd)
+{
+	int freeIndex = GetNextEmptySlot();
+	if (freeIndex == -1)
+	{
+		return false;
+	}
+
+	inventory[freeIndex].item = itemToAdd.item;
+	if (itemToAdd.item.isStackable && itemToAdd.quantity > 1)
+	{
+		inventory[freeIndex].quantity = itemToAdd.quantity;
+		return true;
+	}
+
+	inventory[freeIndex].quantity = 1;
+	return true;
+}
+
+/**
+* Adds the picked up item to an existing stack
+* If there are too many items to fit into the stack, it will recursively try to find other partial stacks
+* If no other partial stacks found, it will try to create a new stack
+*/
+bool UInventoryComponentBase::AddToStack(FInventorySlot itemToAdd, int index)
+{
+	if (itemToAdd.quantity + inventory[index].quantity <= inventory[index].item.maxStackSize)
+	{
+		inventory[index].quantity = inventory[index].quantity + itemToAdd.quantity;
+		return true;
+	}
+	
+	int overflow = (itemToAdd.quantity + inventory[index].quantity) - inventory[index].item.maxStackSize;
+	inventory[index].quantity = inventory[index].item.maxStackSize;
+
+	FInventorySlot overflowItem;
+	overflowItem.item = itemToAdd.item;
+	overflowItem.quantity = overflow;
+
+	if (GetHasPartialStack(itemToAdd))
+	{
+		int partialStackIndex = GetPartialStackIndex(overflowItem);
+		AddToStack(overflowItem, partialStackIndex);
+		return true;
+	}
+
+	// No other partial stacks, try to create a new one
+	if (!CreateStack(overflowItem))
+	{
+		// Could not add the rest of the overflow
+	}
+	
+	return true;
+}
+
+void UInventoryComponentBase::UseQuickSlot1()
+{
+	UseQuickSlot(quickSlot1);
+}
+
+void UInventoryComponentBase::UseQuickSlot2()
+{
+	UseQuickSlot(quickSlot2);
+}
+
+void UInventoryComponentBase::UseQuickSlot3()
+{
+	UseQuickSlot(quickSlot3);
+}
+
+void UInventoryComponentBase::UseQuickSlot(FQuickSlot quickSlot)
+{
+	if (quickSlot.itemIndex < 0)
+	{
+		return;
+	}
+
+	if (quickSlot.item.quantity <= 0)
+	{
+		return;
+	}
+
+	if (UseItem(quickSlot.itemIndex))
+	{
+		if (quickSlot.item.quantity <= 1)
+		{
+			
+		}
+	}
+}
+
+
+void UInventoryComponentBase::RemoveItem(int index)
+{
+	for (int i = index; i < inventory.Num() - 1; ++i)
+	{
+		if (inventory[i + 1].quantity <= 0)
+		{
+			inventory[i] = FInventorySlot();
+			break;
+		}
+
+		inventory[i] = inventory[i + 1];
+	}
+}
+
+/**
+* Gets the index of a partial stack of the queried item
+* returns -1 if no partial stacks are found
+*/
+int UInventoryComponentBase::GetPartialStackIndex(FInventorySlot queryItem)
+{
+	if (!GetHasPartialStack(queryItem))
+	{
+		return -1;
+	}
+
+	for (int i = 0; i < inventory.Num() - 1; ++i)
+	{
+		if (inventory[i].item.itemClass != queryItem.item.itemClass)
+		{
+			continue;
+		}
+
+		if (inventory[i].quantity >= inventory[i].item.maxStackSize)
+		{
+			continue;
+		}
+
+		if (!inventory[i].item.isStackable)
+		{
+			continue;
+		}
+
+		return i;
+	}
+
+	return -1;
+}
+
+/**
+* Checks if the query item already has a partial stack in the inventory
+*/
+bool UInventoryComponentBase::GetHasPartialStack(FInventorySlot queryItem)
+{
+	for (auto item : inventory)
+	{
+		if (item.item.itemClass != queryItem.item.itemClass)
+		{
+			continue;
+		}
+
+		if (!item.item.isStackable)
+		{
+			continue;
+		}
+
+		if (item.quantity < item.item.maxStackSize)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+// Returns -1 if inventory is full
+int UInventoryComponentBase::GetNextEmptySlot()
+{
+	for (int i = 0; i < inventory.Num() - 1; ++i)
+	{
+		if (inventory[i].quantity <= 0)
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+/**
+* Checks if the inventory has an item
+*/
+bool UInventoryComponentBase::GetHasItem(TSubclassOf<class AItemBase> queryItem)
+{
+	for (auto item : inventory)
+	{
+		if (item.item.itemClass == queryItem)
+		{
+			return true;
+		}
+
+		if (item.quantity <= 0)
+		{
+			return false;
+		}
+	}
+
+	return false;
+}
+
+/**
+* Checks if the inventory has a specific item and quantity of item
+*/
+bool UInventoryComponentBase::GetHasItemWithQuantity(TSubclassOf<AItemBase> queryItem, int queryQuantity)
+{
+	int quantityCount = 0;
+	for (auto item : inventory)
+	{
+		if (item.item.itemClass != queryItem)
+		{
+			continue;
+		}
+
+		quantityCount += item.quantity;
+		if (quantityCount >= queryQuantity)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/**
+* Returns a reference to the quick slot.
+* If an index outside of 1 - 3 is passed in, a default quick slot
+* object with slot index of -1 will be returned
+*/
+FQuickSlot UInventoryComponentBase::GetQuickSlot(int quickSlotIndex)
+{
+	switch(quickSlotIndex)
+	{
+		case 1:
+			return quickSlot1;
+
+		case 2:
+			return quickSlot2;
+
+		case 3:
+			return quickSlot3;
+		
+		default:
+			return FQuickSlot();
+	}
 }
--- Source/ProjectMoustache/Public/Interactables/InventoryComponentBase.h
+++ Source/ProjectMoustache/Public/Interactables/InventoryComponentBase.h
@@ -42,6 +42,12 @@ struct FInventorySlot
 {
 	GENERATED_BODY()
 
+	FInventorySlot()
+	{
+		item = FInventoryItem();
+		quantity = 0;
+	}
+
 	// Item struct
 	UPROPERTY(BlueprintReadWrite)
 	FInventoryItem item;
@@ -51,10 +57,64 @@ struct FInventorySlot
 	int quantity;
 };
 
+USTRUCT(BlueprintType)
+struct FQuickSlot
+{
+	GENERATED_BODY()
+
+	FQuickSlot()
+	{
+		itemIndex = -1;
+		item = FInventorySlot();
+	}
+
+	UPROPERTY(BlueprintReadWrite)
+	int itemIndex;
+
+	UPROPERTY(BlueprintReadWrite)
+	FInventorySlot item;
+};
+
 UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class PROJECTMOUSTACHE_API UInventoryComponentBase : public UActorComponent
 {
 	GENERATED_BODY()
+
+	UPROPERTY()
+	UInputComponent* InputComponent;
+
+	FQuickSlot quickSlot1;
+	FQuickSlot quickSlot2;
+	FQuickSlot quickSlot3;
+	
+	/**
+	 * Creates a new stack of items
+	 * Does not add the item if inventory is full
+	 * Adds multiple items if the item is stackable and has more than 1
+	 * Otherwise, just adds one of the item
+	 */
+	bool CreateStack(FInventorySlot itemToAdd);
+	
+	/**
+	 * Adds the picked up item to an existing stack 
+	 */
+	bool AddToStack(FInventorySlot itemToAdd, int index);
+	
+	/**
+	 * Gets the index of a partial stack of the queried item
+	 * returns -1 if no partial stacks are found
+	 */
+	int GetPartialStackIndex(FInventorySlot queryItem);
+	
+	/**
+	 * Checks if the query item already has a partial stack in the inventory
+	 */
+	bool GetHasPartialStack(FInventorySlot queryItem);
+
+	// Returns -1 if inventory is full
+	int GetNextEmptySlot();
+
+	void UseQuickSlot(FQuickSlot quickSlot);
 
 public:	
 	// Sets default values for this component's properties
@@ -72,17 +132,51 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	int numberOfSlots;
 
+	UFUNCTION(BlueprintCallable)
+	void RemoveItem(int index);
+
+	void UseQuickSlot1();
+	void UseQuickSlot2();
+	void UseQuickSlot3();
+	
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	bool UseItem(int index);
+	bool UseItem_Implementation(int index) { return false; }
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	void ToggleInventory();
 	void ToggleInventory_Implementation() { }
 
 	UFUNCTION(BlueprintCallable)
+	bool AddToInventory(FInventorySlot itemToAdd);
+
+	/**
+	 * Checks if the inventory has an item
+	 */
+	UFUNCTION(BlueprintCallable)
+	bool GetHasItem(TSubclassOf<class AItemBase> queryItem);
+
+	/**
+	 * Checks if the inventory has a specific item and quantity of item
+	 */
+	UFUNCTION(BlueprintCallable)
+	bool GetHasItemWithQuantity(TSubclassOf<class AItemBase> queryItem, int queryQuantity);
+
+	UFUNCTION(BlueprintCallable)
 	int GetInventorySize() { return inventory.Num(); }
 
 	UFUNCTION(BlueprintCallable)
 	TArray<FInventorySlot> GetInventory() { return inventory; }
+
+	/**
+	 * Returns a reference to the quick slot.
+	 * If an index outside of 1 - 3 is passed in, a default quick slot
+	 * object with slot index of -1 will be returned
+	 */
+	UFUNCTION(BlueprintCallable)
+	FQuickSlot GetQuickSlot(int quickSlotIndex);
 };
