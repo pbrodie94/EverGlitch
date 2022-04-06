da05f595f8d0bc5ea80385c23ef87682cc2b627c
pbrodie94 <82655776+pbrodie94@users.noreply.github.com>
2022-04-06 14:39:33 -0400

    Entity Base Class
    Added entity base class to handle like functionality for all game characters (Player and enemies)
    - Handles health
    - Handles status effects
    - Handles element effectiveness
    - All enemies and Player will inherit

---
+++ Source/ProjectMoustache/Private/EntityBase.cpp
@@ -0,0 +1,347 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EntityBase.h"
+
+#include "StatusEffect.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+AEntityBase::AEntityBase()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	maxHealth = 100;
+
+	fireEffectiveness = 0;
+	iceEffectiveness = 0;
+	lightningEffectiveness = 0;
+	waterEffectiveness = 0;
+}
+
+// Called when the game starts or when spawned
+void AEntityBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (maxHealth <= 1)
+	{
+		maxHealth = 100;
+	}
+
+	currentHealth = startingHealth > 0 ? startingHealth : maxHealth;
+
+	fireEffectiveness = FMath::Clamp(fireEffectiveness, -100.0f, 200.0f);
+	iceEffectiveness = FMath::Clamp(iceEffectiveness, -100.0f, 200.0f);
+	lightningEffectiveness = FMath::Clamp(lightningEffectiveness, -100.0f, 200.0f);
+	waterEffectiveness = FMath::Clamp(waterEffectiveness, -100.0f, 200.0f);
+}
+
+// Called every frame
+void AEntityBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Remove old status effects
+	if (removedStatusEffects.Num() > 0)
+	{
+		for (UStatusEffectBase* removedEffect : removedStatusEffects)
+		{
+			statusEffects.Remove(removedEffect);
+		}
+
+		removedStatusEffects.Empty();
+	}
+	
+	// Add new status effects
+	if (newStatusEffects.Num() > 0)
+	{
+		for (UStatusEffectBase* newEffect : newStatusEffects)
+		{
+			statusEffects.Add(newEffect);
+		}
+
+		newStatusEffects.Empty();
+	}
+	
+	// Update current status effects
+	if (statusEffects.Num() > 0)
+	{
+		for (UStatusEffectBase* statusEffect : statusEffects)
+		{
+			statusEffect->UpdateStatus();
+		}
+	}
+}
+
+/*********************************************************************
+* IDamageable
+*********************************************************************/
+float AEntityBase::TakeIncomingDamage_Implementation(float damageAmount, AActor* damageCauser, AController* eventInstigator, FDamageData damageData)
+{
+	if (damageAmount <= 0 || currentHealth <= 0)
+	{
+		return 0;
+	}
+
+	float damage = damageAmount;
+
+	// Adjust damage based on status effects
+	if (damageData.statusEffect.statusEffectType != None)
+	{
+		const FStatusEffect elementEffect = damageData.statusEffect;
+		switch (elementEffect.statusEffectType)
+		{
+		case Burn:
+			damage *= (fireEffectiveness / 100);
+			break;
+		case Chilled:
+			damage *= (iceEffectiveness / 100);
+			break;
+		case Wet:
+			damage *= (waterEffectiveness / 100);
+			break;
+		case Static:
+			damage *= (lightningEffectiveness / 100);
+
+			// Add damage increase if wet
+			if (GetHasStatusEffect(Wet))
+			{
+				UWetStatus* wetStatus = Cast<UWetStatus>(GetStatusEffect(Wet));
+				if (wetStatus != nullptr)
+				{
+					damage *= (1 + (wetStatus->GetEffectAmount() / 100));
+				}
+			}
+			
+			break;
+		default:
+			break;
+		}
+
+		// Determine if status effect is applied
+		if (elementEffect.statusInflictChance > 0 && elementEffect.duration > 0)
+		{
+			const float rand = FMath::RandRange(0, 100);
+			if (rand < elementEffect.statusInflictChance)
+			{
+				AddStatusEffect(elementEffect);
+			}
+		}
+	}
+
+	currentHealth -= damage;
+	if (currentHealth <= 0)
+	{
+		currentHealth = 0;
+		Die();
+	}
+	
+	return damage;
+}
+
+/**
+* Takes in a status effect struct and adds it to the character
+* If the resistance to the element is <= 0, the effect will not be added
+*/
+void AEntityBase::AddStatusEffect_Implementation(FStatusEffect statusEffect)
+{
+	UStatusEffectBase* status = nullptr;
+	
+	switch (statusEffect.statusEffectType)
+	{
+		case Burn:
+
+			if (GetHasStatusEffect(Wet))
+			{
+				RemoveStatus(Wet);
+				return;
+			}
+			
+			if (fireEffectiveness <= 0)
+			{
+				return;
+			}
+
+			// Check if already have burn status
+			if (GetHasStatusEffect(Burn))
+			{
+				// Check if burn status has less time left than new status
+				UStatusEffectBase* burnStatus = GetStatusEffect(Burn);
+				if (burnStatus != nullptr)
+				{
+					if (burnStatus->GetTimeRemaining() > statusEffect.duration)
+					{
+						return;
+					}
+				}
+
+				// Remove then add new status
+				RemoveStatus(Burn);
+			}
+			
+			status = NewObject<UBurnStatus>();
+		
+			break;
+		case Chilled:
+
+			if (iceEffectiveness <= 0)
+			{
+				return;
+			}
+
+			// Check if has chilled status
+			if (GetHasStatusEffect(Chilled))
+			{
+				UStatusEffectBase* chilledStatus = GetStatusEffect(Chilled);
+				if (chilledStatus != nullptr)
+				{
+					if (chilledStatus->GetTimeRemaining() > statusEffect.duration)
+					{
+						return;
+					}
+				}
+
+				RemoveStatus(Chilled);
+			}
+
+			status = NewObject<UChilledStatus>();
+			
+			break;
+		case Static:
+
+			if (lightningEffectiveness <= 0)
+			{
+				return;
+			}
+
+			// Check if has static status
+			if (GetHasStatusEffect(Static))
+			{
+				UStatusEffectBase* staticStatus = GetStatusEffect(Static);
+				if (staticStatus != nullptr)
+				{
+					if (staticStatus->GetTimeRemaining() > statusEffect.duration)
+					{
+						return;
+					}
+				}
+
+				RemoveStatus(Static);
+			}
+
+			status = NewObject<UStaticEffect>();
+			
+			break;
+		case Wet:
+
+			if (waterEffectiveness <= 0)
+			{
+				return;
+			}
+
+			// Check if has wet status
+			if (GetHasStatusEffect(Wet))
+			{
+				UStatusEffectBase* wetStatus = GetStatusEffect(Wet);
+				if (wetStatus != nullptr)
+				{
+					if (wetStatus->GetTimeRemaining() > statusEffect.duration)
+					{
+						return;
+					}
+
+					RemoveStatus(Wet);
+				}
+			}
+		
+			status = NewObject<UWetStatus>();
+			
+			break;
+		case Frozen:
+			break;
+		default:
+			break;
+	}
+
+	if (status == nullptr)
+	{
+		return;
+	}
+	
+	status->Init(this, statusEffect.effectAmount, statusEffect.duration, statusEffect.dotInterval);
+	newStatusEffects.Add(status);
+}
+
+/**
+* Removes all status effects of a type
+*/
+void AEntityBase::RemoveStatus_Implementation(EStatusEffectType statusEffect)
+{
+	if (statusEffects.Num() <= 0)
+	{
+		return;
+	}
+
+	for (UStatusEffectBase* status : statusEffects)
+	{
+		if (status->GetEffectType() == statusEffect)
+		{
+			status->SetIsExpired(true);
+			removedStatusEffects.Add(status);
+		}
+	}
+}
+
+/**
+* Called by a status effect to remove themselves
+*/
+void AEntityBase::RemoveStatusEffect_Implementation(UStatusEffectBase* statusEffect)
+{
+	if (statusEffects.Num() <= 0)
+	{
+		return;
+	}
+
+	statusEffect->SetIsExpired(true);
+	removedStatusEffects.Add(statusEffect);
+}
+
+/**
+* Returns the first found non-expired object for the inputted status effect type
+* If the status effect does not exist, it will return nullptr
+*/
+UStatusEffectBase* AEntityBase::GetStatusEffect(EStatusEffectType effectType)
+{
+	if (statusEffects.Num() <= 0)
+	{
+		return nullptr;
+	}
+
+	for (UStatusEffectBase* status : statusEffects)
+	{
+		if (status->GetEffectType() == effectType && !status->GetIsExpired())
+		{
+			return status;
+		}
+	}
+
+	return nullptr;
+}
+
+/**
+* Returns true if the character has the status effect
+*/
+bool AEntityBase::GetHasStatusEffect_Implementation(EStatusEffectType statusEffect)
+{
+	for (UStatusEffectBase* status : statusEffects)
+	{
+		if (status->GetEffectType() == statusEffect && !status->GetIsExpired())
+		{
+			return true;
+		}
+	}
+	
+	return false;
+}
---
+++ Source/ProjectMoustache/Public/EntityBase.h
@@ -0,0 +1,183 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Damageable.h"
+#include "GameFramework/Character.h"
+#include "EntityBase.generated.h"
+
+UCLASS()
+class PROJECTMOUSTACHE_API AEntityBase : public ACharacter, public IDamageable
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TArray<class UStatusEffectBase*> newStatusEffects;
+	UPROPERTY()
+	TArray<class UStatusEffectBase*> statusEffects;
+	UPROPERTY()
+	TArray<class UStatusEffectBase*> removedStatusEffects;
+
+public:
+	// Sets default values for this character's properties
+	AEntityBase();
+
+protected:
+
+	// Health the enemy starts with, defaults to max health
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Stats)
+	float startingHealth;
+
+	// Maximum possible health, if less than 1: defaults to 100
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Stats)
+	float maxHealth;
+
+	// Current health
+	UPROPERTY(BlueprintReadWrite)
+	float currentHealth;
+
+	// Element effectiveness
+	/**
+	* Effectiveness of fire damage on the Entity
+	* Effectiveness is clamped between -100% - 200%
+	* 0% negates all damage
+	* 200% deals double damage
+	* -100% adds all damage back to health
+	*/
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Combat)
+	float fireEffectiveness;
+
+	/**
+	* Effectiveness of ice damage on the Entity
+	* Effectiveness is clamped between -100% - 200%
+	* 0% negates all damage
+	* 200% deals double damage
+	* -100% adds all damage back to health
+	*/
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Combat)
+	float iceEffectiveness;
+
+	/**
+	* Effectiveness of lightning damage on the Entity
+	* Effectiveness is clamped between -100% - 200%
+	* 0% negates all damage
+	* 200% deals double damage
+	* -100% adds all damage back to health
+	*/
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Combat)
+	float lightningEffectiveness;
+
+	/**
+	* Effectiveness of water damage on the Entity
+	* Effectiveness is clamped between -100% - 200%
+	* 0% negates all damage
+	* 200% deals double damage
+	* -100% adds all damage back to health
+	*/
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Combat)
+	float waterEffectiveness;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void Die();
+	virtual void Die_Implementation() { }
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/*********************************************************************
+	 * IDamageable
+	 ********************************************************************/
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	float TakeIncomingDamage(float damageAmount, AActor* damageCauser, AController* eventInstigator, FDamageData damageData);
+	virtual float TakeIncomingDamage_Implementation(float damageAmount, AActor* damageCauser, AController* eventInstigator, FDamageData damageData) override;
+
+	/**
+	* Takes in a status effect struct and adds it to the character
+	* If the resistance to the element is <= 0, the effect will not be added
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void AddStatusEffect(FStatusEffect statusEffect);
+	virtual void AddStatusEffect_Implementation(FStatusEffect) override;
+
+	/**
+	* Removes all status effects of a type
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void RemoveStatus(EStatusEffectType statusEffect);
+	virtual void RemoveStatus_Implementation(EStatusEffectType StatusEffect) override;
+
+	/**
+	 * Called by a status effect to remove themselves
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void RemoveStatusEffect(class UStatusEffectBase* statusEffect);
+	virtual void RemoveStatusEffect_Implementation(class UStatusEffectBase* statusEffect) override;
+
+	/**
+	 * Returns the first found non-expired object for the inputted status effect type
+	 * If the status effect does not exist, it will return nullptr
+	 */
+	class UStatusEffectBase* GetStatusEffect(EStatusEffectType effectType);
+	
+	/**
+	* Returns character's resistance to fire
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	float GetFireResistance() const;
+	virtual FORCEINLINE float GetFireResistance_Implementation() const override { return fireEffectiveness; }
+
+	/**
+	* Returns character's resistance to ice
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	float GetIceResistance() const;
+	virtual FORCEINLINE float GetIceResistance_Implementation() const override { return iceEffectiveness; }	
+
+	/**
+	* Returns character's resistance to water
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	float GetWaterResistance() const;
+	virtual FORCEINLINE float GetWaterResistance_Implementation() const override { return waterEffectiveness; }
+
+	/**
+	* Returns character's resistance to lightning
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	float GetLightningResistance() const;
+	virtual FORCEINLINE float GetLightningResistance_Implementation() const override { return 0; }
+	
+	/**
+	* Returns true if the character has the status effect
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	bool GetHasStatusEffect(EStatusEffectType statusEffect);
+	virtual bool GetHasStatusEffect_Implementation(EStatusEffectType statusEffect) override;
+
+	/**
+	* Returns the current amount of health
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	float GetCurrentHealth() const;
+	virtual FORCEINLINE float GetCurrentHealth_Implementation() const override { return currentHealth; }
+
+	/**
+	* Returns the max health the character can have
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	float GetMaxHealth() const;
+	virtual FORCEINLINE float GetMaxHealth_Implementation() const override { return maxHealth; }
+
+	/**
+	* Returns true if the character is dead
+	* i.e. the characters health is <= 0
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	bool GetIsDead() const;
+	virtual FORCEINLINE bool GetIsDead_Implementation() const override { return currentHealth <= 0; }
+};
